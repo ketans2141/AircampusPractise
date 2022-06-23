f2e9350a356932eee0184d71c328de8243f9fafe
Ketan <ketanshetge2022@gmail.com>
2022-06-23 20:20:42 +0530

    numbers from 1-n

--- Recursion_Questions/recursion.js
+++ Recursion_Questions/recursion.js
@@ -8,3 +8,13 @@ function fibo(first, second, counter){
     console.log(first);
 }
 fibo(0,1,1);
+
+// print all the numbers between given range 
+
+function number(num1,num2){
+    if(num1<=num2){
+        console.log(num1);
+        return number(num1+1,num2);
+    }
+}
+number(1,10);
