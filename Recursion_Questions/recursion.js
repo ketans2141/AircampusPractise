1653401abfb40b9cebaf76c768c6d22b87047bfa
Ketan <ketanshetge2022@gmail.com>
2022-06-23 20:24:57 +0530

    numbers from n to 1

--- Recursion_Questions/recursion.js
+++ Recursion_Questions/recursion.js
@@ -9,7 +9,7 @@ function fibo(first, second, counter){
 }
 fibo(0,1,1);
 
-// print all the numbers between given range 
+// Given a number n, print numbers from 1 to n using recursion.
 
 function number(num1,num2){
     if(num1<=num2){
@@ -18,3 +18,18 @@ function number(num1,num2){
     }
 }
 number(1,10);
+
+
+// Given a number n, print numbers from n to 1 using recursion.
+
+function number(num1,num2){
+    if(num1>=num2){
+        console.log(num1);
+        return number(num1-1,num2);
+    }
+}
+number(10,1);
+
+
+// Print factorial of a number using recursion.
+
