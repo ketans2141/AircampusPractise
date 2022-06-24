2f8c726a64cf142c81430ef8cc5200c421f6083d
Ketan <ketanshetge2022@gmail.com>
2022-06-24 13:03:52 +0530

    Qst5 done

--- 24th-June/Recursion_Assignment/Recursion.js
+++ 24th-June/Recursion_Assignment/Recursion.js
@@ -1,45 +1,57 @@
-// Print all the numbers in the given range. 
+// // Print all the numbers in the given range. 
 
-function numberRange(num1,num2){
-    if(num1<=num2){
-        console.log(num1);
-        numberRange(num1+1,num2);
+// function numberRange(num1,num2){
+//     if(num1<=num2){
+//         console.log(num1);
+//         numberRange(num1+1,num2);
+//     }
+// }
+// numberRange(1,10);
+
+// // Print all the numbers in descending order in the given range 
+
+// function numberDescending(num1,num2){
+//     if(num1>=num2){
+//         console.log(num1);
+//         numberDescending(num1-1,num2);
+//     }
+// }
+// numberDescending(10,1);
+
+
+// // Print all the even numbers in the given range using recursion
+
+// function evenNumbers(num1,num2){
+//     if(num1<=num2){
+//         if(num1%2==0){
+//             console.log(num1);
+//         }
+//         evenNumbers(num1+1,num2);
+//     }
+// }
+// evenNumbers(1,10);
+
+
+// // Find the factorial of a given number n. 
+
+// function facto(num1,fact){
+//     if(num1>=1){
+//         fact=fact*num1;
+//         return facto(num1-1,fact);
+//     }
+//     return fact;
+// }
+// console.log(facto(4,1));
+
+
+// Find the nth digit of the fibonacci series. 
+
+function fibo(a,b,counter){
+    if(counter<=10){
+        let c=a+b;
+        return fibo(b,c,counter+1);
     }
+    console.log(a);
 }
-numberRange(1,10);
-
-// Print all the numbers in descending order in the given range 
-
-function numberDescending(num1,num2){
-    if(num1>=num2){
-        console.log(num1);
-        numberDescending(num1-1,num2);
-    }
-}
-numberDescending(10,1);
-
-
-// Print all the even numbers in the given range using recursion
-
-function evenNumbers(num1,num2){
-    if(num1<=num2){
-        if(num1%2==0){
-            console.log(num1);
-        }
-        evenNumbers(num1+1,num2);
-    }
-}
-evenNumbers(1,10);
-
-
-// Find the factorial of a given number n. 
-
-function facto(num1,fact){
-    if(num1>=1){
-        fact=fact*num1;
-        return facto(num1-1,fact);
-    }
-    return fact;
-}
-console.log(facto(4,1));
+fibo(0,1,1);
   
--- 24th-June/Recursion_Assignment/readme.md
+++ 24th-June/Recursion_Assignment/readme.md
@@ -3,3 +3,4 @@
 <img src="qst2.png" alt="output">
 <img src="qst3.png" alt="output">
 <img src="qst4.png" alt="output">
+<img src="qst5.png" alt="output">
