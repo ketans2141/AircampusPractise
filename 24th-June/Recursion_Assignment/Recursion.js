bf49f192c9f705815049e0fa66e500132d38f258
Ketan <ketanshetge2022@gmail.com>
2022-06-24 12:13:28 +0530

    Qst1 done

---
+++ 24th-June/Recursion_Assignment/Recursion.js
@@ -0,0 +1,33 @@
+// Print all the numbers in the given range. 
+
+function numberRange(num1,num2){
+    if(num1<=num2){
+        console.log(num1);
+        numberRange(num1+1,num2);
+    }
+}
+numberRange(1,10);
+
+// Print all the numbers in descending order in the given range 
+
+// function numberDescending(num1,num2){
+//     if(num1>=num2){
+//         console.log(num1);
+//         numberDescending(num1-1,num2);
+//     }
+// }
+// numberDescending(10,1);
+
+
+// Print all the even numbers in the given range using recursion
+
+// function evenNumbers(num1,num2){
+//     if(num1<=num2){
+//         if(num1%2==0){
+//             console.log(num1);
+//         }
+//         evenNumbers(num1+1,num2);
+//     }
+// }
+// evenNumbers(1,10);
+  
---
+++ 24th-June/Recursion_Assignment/readme.md
@@ -0,0 +1,2 @@
+<h1>Output</h1>
+<img src="qst1.png" alt="output">
