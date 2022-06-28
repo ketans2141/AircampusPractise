6cffcbdd4f1d131cc6672684d0357766ba00bd71
Ketan <ketanshetge2022@gmail.com>
2022-06-28 12:58:11 +0530

    loops practise

---
+++ 28th-June/index.js
@@ -0,0 +1,21 @@
+// let num=5;
+// for(let i=1;i<=num;i++){
+//     let star="";
+//     for(let j=1;j<=num;j++){
+//         star=star+"*";
+//     }
+//     console.log(star);
+// }
+
+
+// ######################################################################################################################
+
+
+let num=5;
+for(let i=1;i<=num;i++){
+    let star="";
+    for(let j=1;j<=i;j++){
+        star=star+"*";
+    }
+    console.log(star);
+}
