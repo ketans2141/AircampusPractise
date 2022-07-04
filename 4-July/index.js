eb7c22ac4374269a7d3da8a0e259a949fc0be5fd
Ketan <ketanshetge2022@gmail.com>
2022-07-04 20:49:46 +0530

    JS Test Practise

---
+++ 4-July/index.js
@@ -0,0 +1,93 @@
+// Take an integer and print 'aircampus' if the input integer is an Aircampus number, otherwise print 'no'.
+
+
+
+// function aircampus(num){
+// // let num=145;
+// let count=0;
+// let sum=0;
+// let temp=num;
+
+// while(num!=0){
+//     let digit=num%10;
+//     count=count+1;
+//     num=Math.floor(num/10);
+// }
+// // console.log(count);
+
+// num=temp;
+// while(num!=0){
+//     let digit=num%10;
+//     sum=sum+digit**count;
+//     num=Math.floor(num/10);
+// }
+// // console.log(sum);
+
+// if(temp==sum){
+//     return "aircampus";
+// }
+// else{
+//     return "no";
+// }
+// }
+// console.log(aircampus(145))
+
+
+
+
+
+// 5
+// 4 5
+// 3 4 5
+// 2 3 4 5
+// 1 2 3 4 5
+
+// find sum of all numbers in pattern
+
+// let sum=0;
+// let n=5
+
+// for(let i=n;i>=1;i--){
+//   let str="";
+//   for(let j=i;j<=n;j++){
+//     str=str+`${j} `;
+//     sum=sum+j;
+//   }
+// }
+// console.log(sum);
+
+// for(let i=5;i>=1;i++)
+
+
+// // Take an integer as input and print 'YES' if the number is a special number. Otherwise, print 'NO'.
+// let num=145;
+// let count=0;
+// let fact=1;
+// let sum=0;
+// let temp=num;
+// if(num==0){
+//     return "No";
+// }
+// while(num!=0){
+//     let digit=num%10;
+//     count=count+1;
+//     sum=sum+fact;
+//     num=(Math.floor(num/10));
+//     // console.log(digit);   
+// }
+// num=temp;
+// for(let i=2;i<=num;i++){
+//     fact=fact*i;
+// }
+// console.log(fact);
+
+
+function getName(){
+    return "Ketan";
+}
+console.log("Ketan")
+let x=getName();
+console.log(x);
+let y=5;
+console.log(5);
+
