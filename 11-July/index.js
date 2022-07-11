e816bf7d4d5525407e043b135041efa0ec548eef
Ketan <ketanshetge2022@gmail.com>
2022-07-11 22:50:24 +0530

    Basics Array

---
+++ 11-July/index.js
@@ -0,0 +1,33 @@
+var numbers = [1,2,3,4,5,6,7,8];
+numbers.forEach(function(element, index, array){
+    array[index] = element* element;
+});
+console.log(numbers);
+
+
+// function square(arr) {
+
+//     str= [];
+
+//     for (var i = 0; i < arr.len; i++) {
+//         str.push(arr[i] * arr[i]);
+//     }
+
+//     return str;     
+// }
+// console.log(square([1,2,3,4]));
+
+
+// function square(arr){
+//     let sqr=arr.map(num=>num**2);
+//     return sqr;
+// }
+// console.log(square([1,2,3,4,5]));
+
+
+// function goodManager(arr){
+//     return arr.map(function incSal(Sal){
+//       return Sal+1000;
+//     })
+//     }
+//     console.log(goodManager([1000,2000,3000]));
