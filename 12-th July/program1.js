58e8d913660505e6a10c340daa0ce9ecf8d8aada
Ketan <ketanshetge2022@gmail.com>
2022-07-12 14:35:57 +0530

    Basics Array

---
+++ 12-th July/program1.js
@@ -0,0 +1,51 @@
+// to find first index of given Element 
+
+let arr=[1,2,5,3,2,6];
+
+console.log(arr.indexOf(5));
+
+
+// last index of element 
+
+let array=[1,3,4,2,5,3,2];
+
+console.log(array.lastIndexOf(3));
+
+
+// First non-repeating number 
+
+// let nums=[1,4,3,5,6,2,3];
+
+// for(let i=0;i<nums.length;i++){
+//     if(nums.indexOf(nums[i])==nums.lastIndexOf(nums[i])){
+//         console.log(nums[i]);
+//     }
+// }
+
+// using function first non- repeating number
+// let num=[1,2,1,2,1,3,4];
+// function nonRepeating(num){
+//     for(let i=0;i<num.length;i++){
+//         if(num.indexOf(num[i])==num.lastIndexOf(num[i])) return num[i];
+//     }
+// }
+// console.log(nonRepeating(num));
+
+
+// arr=[2,4,6,8];
+
+// let sqr=arr.filter(function(num){
+//     return (num**2>15)
+// })
+// console.log(sqr);
+
+
+// arr=[2,4,6,8];
+
+// let sqr=arr.map(function(num){
+//     return num**2;
+// })
+// // return sqr;
+// console.log(sqr);
+
+
