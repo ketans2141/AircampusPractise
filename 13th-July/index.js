eac1f3424304a32a565c4f077216ee16f4adf349
Ketan <ketanshetge2022@gmail.com>
2022-07-13 19:02:42 +0530

    Sum of matrix

--- 13th-July/index.js
+++ 13th-July/index.js
@@ -54,16 +54,65 @@
 
 
 
-let obj={
-    name:"Ketan",
-    details:{
-        id:1234,
-        city:"Mumbai"
+// let obj={
+//     name:"Ketan",
+//     details:{
+//         id:1234,
+//         city:"Mumbai"
 
+//     }
+// };
+
+// let user=JSON.parse(JSON.stringify(obj));
+// user.details.id=4567;
+
+// console.log(obj);
+
+
+
+// 2D Array Basics 
+
+let matrix=[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+
+let rows=matrix.length
+// console.log(rows);
+
+let cols= matrix[0].length;
+// console.log(cols);
+let sum=0;
+for(let i=0;i<rows;i++){
+    for(let j=0;j<cols;j++){
+        if(i==j){
+        sum= sum+ matrix[i][j];
+
+        }
     }
-};
+}
+console.log(sum);
 
-let user=JSON.parse(JSON.stringify(obj));
-user.details.id=4567;
+// i=0    j=0,1,2,3,4    sum=1,3,6,10
 
-console.log(obj);
+//i=1      j=0,          sum=
+
+
+let array=[
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+]
+
+let row=array.length;
+let col=array[0].length;
+
+let add=0;
+
+for(let i=0; i<row; i++){
+    add=add+array[i][i];
+}
+console.log(add);
