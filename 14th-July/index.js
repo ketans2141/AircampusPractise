b2b6e0e1c1d594e740e82dbe9c3e273815a46c78
Ketan <ketanshetge2022@gmail.com>
2022-07-14 17:01:26 +0530

    shallow copy & template literals

--- 14th-July/index.js
+++ 14th-July/index.js
@@ -1,8 +1,25 @@
 // spread operator 
 // Extract the values from the array 
 
-let n1=[1,2,3];
-let n2=[5,6,7];
-let n3=[9,10,11];
+// let n1=[1,2,3];
+// let n2=[5,6,7];
+// let n3=[9,10,11];
 
-console.log([...n1,'Ketan',...n2,...n3])
+// console.log([...n1,'Ketan',...n2,...n3])
+
+
+// shallow copy 
+let num1=[1,2,3];
+// console.log(num1)
+let num2=[...num1];
+
+num2[0]=5;
+console.log(num1,num2);
+
+
+// template literals 
+
+let fname="Ketan";
+let lname="Shetge";
+
+console.log(`"${fname}" '${lname}'`)
