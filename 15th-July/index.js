9598ce49a77cbef5d0b825ce9fabb0cd82b2b1c5
Ketan <ketanshetge2022@gmail.com>
2022-07-15 21:43:30 +0530

    for_of

--- 15th-July/index.js
+++ 15th-July/index.js
@@ -1,14 +1,29 @@
 // anagram 
 
-let word1="mad";
-let word2="dam";
+// let word1="mad";
+// let word2="dam";
 
-word1=word1.split('').sort()
-// console.log(word1);
-word2=word2.split('').sort()
+// word1=word1.split('').sort()
+// // console.log(word1);
+// word2=word2.split('').sort()
 
-if(word1.join('')==word2.join('')){
-    console.log('Word is Anagram');
-}else{
-    console.log('Word is not an Anagram')
+// if(word1.join('')==word2.join('')){
+//     console.log('Word is Anagram');
+// }else{
+//     console.log('Word is not an Anagram')
+// }
+
+
+// for of
+
+let nums=[1,2,3,4];
+// normal for loop 
+for(let i=0;i<nums.length;i++){          
+    console.log(nums[i]);
 }
+
+for(let num of nums){
+    console.log(num);
+}
+
+
--- 15th-July/readme.md
+++ 15th-July/readme.md
@@ -1,2 +1,3 @@
 <h1>Output</h1>
 <img src="Anagram.png" alt="one">
+<img src="for_of.png" alt="one">
