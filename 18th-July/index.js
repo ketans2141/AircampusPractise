11b01137508e40e9a75a7b4a6c0f9f06d0e4cb03
Ketan <ketanshetge2022@gmail.com>
2022-07-18 18:30:51 +0530

    neighbour  test ques

--- 18th-July/index.js
+++ 18th-July/index.js
@@ -16,3 +16,26 @@ for(let i=0;i<arr.length;i++){
     }
 }
 console.log(array);
+
+
+// Mary wants to distribute pens to all the kids in her neighbourhood. Can you help her by creating an array where all the kids except her neighbour get 'm' number of pens and her neighbour kids get 'm+5'. Please note she doesn't have a kid.
+
+
+let m=5;
+let n=10;
+let pos=2;
+
+let newArray=[];
+
+for(let i=0;i<n;i++){
+    if(i==pos){
+        newArray[i]=0;
+    }
+    else if(i==pos-1 || i==pos+1){
+        newArray[i]=m+5;
+    }
+    else{
+        newArray.push(m);
+    }
+}
+console.log(newArray);
--- 18th-July/readme.md
+++ 18th-July/readme.md
@@ -1,2 +1,3 @@
 <h1>Output</h1>
 <img src="arrayOperation.png" alt="one">
+<img src="neighboursques" alt="one">
