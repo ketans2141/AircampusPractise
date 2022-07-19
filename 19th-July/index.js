f88413640be21285573e9f65301554f54eca96eb
Ketan <ketanshetge2022@gmail.com>
2022-07-19 13:01:10 +0530

    object practise

--- 19th-July/index.js
+++ 19th-July/index.js
@@ -5,12 +5,30 @@
 // }
 // console.log(Maharashtra["city"],Maharashtra.place);
 
-let maharashtra={
-    city:"Mumbai",
-    place:"Dadar",
-   details:{
-    name:"Ketan",
-    age:"22",
-   }
-}
-console.log(maharashtra["city"],maharashtra.details.name,maharashtra["details"]);
+// let maharashtra={
+//     city:"Mumbai",
+//     place:"Dadar",
+//    details:{
+//     name:"Ketan",
+//     age:"22",
+//    }
+// }
+// let info="details";
+// console.log(maharashtra["city"],maharashtra.details.name,maharashtra["details"]);
+// console.log(maharashtra[info],maharashtra[info]["age"])
+
+// adding function inside object 
+
+let square={
+    side1:4,
+    side2:3,
+    side3:6,
+    side4:2,
+    area:{
+        formula:"side**2",
+    },
+    draw: function (){
+        return "drawing circle";
+    },
+};
+console.log(square.draw())
