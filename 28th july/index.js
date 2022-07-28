5c7564534f660a6d48e8e213777b349ac67d1288
Ketan <ketanshetge2022@gmail.com>
2022-07-28 17:22:32 +0530

    string basic problem

--- 28th july/index.js
+++ 28th july/index.js
@@ -11,3 +11,23 @@ function findMin(arr){
     return min;
 }
 console.log(findMin([4,2,8,1,9]))
+
+
+// let str="ketan"  if t is at 3rd position return true else false 
+
+function check(str){
+
+if(str[2]=='t'){
+    return "true";
+}else return false;
+}
+console.log(check("ketan"))
+
+
+//string="My name is ketan"   check if  is present in string
+
+// function isCheck(string){
+//     if(string.includes("ketan"))return "Yes";
+//     else return "No"
+// }
+// console.log(isCheck("My name is ketan"))
--- 28th july/readme.md
+++ 28th july/readme.md
@@ -1,2 +1,3 @@
 <h1>Output</h1>
 <img src="min.png" alt="one">
+<img src="string.png" alt="one">
