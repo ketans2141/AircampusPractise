997943a039300f3578a6686e786fd720b6a83597
ketans2141 <105305997+ketans2141@users.noreply.github.com>
2022-08-25 14:30:03 +0530

    Updated code

--- 4th-August/index.js
+++ 4th-August/index.js
@@ -1,10 +1,22 @@
 // first index 
 
-function firstIndex(arr,num){
-    for(let i of arr){
-        if(arr[i]==num){
-            return i;
-        }
+// function firstIndex(arr,num){
+//     for(let i of arr){
+//         if(arr[i]==num){
+//             return i;
+//         }
+//     }
+// }
+// console.log(firstIndex([1,2,3,4],4))
+
+// reverse array 
+
+function reverse(arr){
+    let reversed=[];
+    for(let i=arr.length-1;j=0,i>=0;i--,j++){
+        reversed[j]=arr[i];
     }
+    return reversed;
 }
-console.log(firstIndex([1,2,3,4],4))
+console.log(reverse([3,2,1,5]));
+
